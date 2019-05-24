4bfeb00b5590b394fbf53b50fa504f6177df8920
Nik Vanderhoof <nikolasrvanderhoof@gmail.com>
2019-05-24 15:40:52 -0400

    Remove spark logger from unit tests

--- spark-constraints/test/src/com/nikvanderhoof/spark/sql/constraints/UtestSparkSession.scala
+++ spark-constraints/test/src/com/nikvanderhoof/spark/sql/constraints/UtestSparkSession.scala
@@ -1,13 +1,19 @@
 package com.nikvanderhoof.spark.sql.constraints
 
 import org.apache.spark.sql.SparkSession
+import org.apache.log4j.{Logger, Level}
 import utest._
 
 trait UtestSparkSession { self: TestSuite =>
-  val spark = SparkSession.builder().master("local[*]").appName("Tests").config("spark.sql.shuffle.partitions", "1").getOrCreate()
+  Logger.getLogger("org").setLevel(Level.OFF)
+  val spark = SparkSession
+    .builder()
+    .master("local[*]")
+    .appName("Tests")
+    .config("spark.sql.shuffle.partitions", "1")
+    .getOrCreate()
   val sc = spark.sparkContext
   val sql = spark.sqlContext
-  sc.setLogLevel("ERROR")
   override def utestAfterAll(): Unit = {
     spark.close()
   }
