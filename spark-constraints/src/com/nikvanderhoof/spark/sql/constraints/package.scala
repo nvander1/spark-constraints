889c3e83ffab8b2d116134a26c3909baa3fd302b
Nik Vanderhoof <nikolasrvanderhoof@gmail.com>
2019-05-24 15:46:49 -0400

    Remove unused dependencies and imports

--- spark-constraints/src/com/nikvanderhoof/spark/sql/constraints/package.scala
+++ spark-constraints/src/com/nikvanderhoof/spark/sql/constraints/package.scala
@@ -1,13 +1,7 @@
 package com.nikvanderhoof.spark.sql
 
-import scala.annotation.compileTimeOnly
-import scala.language.experimental.macros
-import scala.reflect.macros.whitebox.Context
-
-import com.github.dwickern.macros.NameOfImpl
 import org.apache.spark.sql.{Column, Dataset}
 import org.apache.spark.sql.constraints.datasetToNamedDataset
-import org.apache.spark.sql.functions.col
 
 package object constraints {
   implicit def dataset2NamedDataset[T](dataset: Dataset[T]) = datasetToNamedDataset(dataset)
